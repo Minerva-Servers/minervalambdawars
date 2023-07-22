6fd9ebf2df566ab08f11ae4bec096940445fea9c
Riggs <49407096+riggs9162@users.noreply.github.com>
2023-07-22 14:04:45 +0200

    Delete minerva-lambdawars directory

--- minerva-lambdawars/python/wars_game/buildings/__init__.py
+++
@@ -1,2 +0,0 @@
-from . import combine
-from . import rebels
--- minerva-lambdawars/python/wars_game/hud/__init__.py
+++
@@ -1,5 +0,0 @@
-if isclient:
-    from .overrun import HudOverrun
-    from .capturetheflag import HudCaptureTheFlag
-    from .scrap import HudBuildScrap
-    from .powerextractor import HudBuildPowerExtractor
